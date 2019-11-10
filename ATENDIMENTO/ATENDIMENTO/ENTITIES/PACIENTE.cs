adfe38bb0a5f27df6b05c8819309b2425341694c
DIEGO DA SILVA MILHOMEM <diegosmilhomem23@hotmail.com>
2019-11-10 19:39:35 -0300

    SUBINDO PROJETO DE LTP5

---
+++ ATENDIMENTO/ATENDIMENTO/ENTITIES/PACIENTE.cs
@@ -0,0 +1,41 @@
+﻿using SHAREDCONTEXT;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace ATENDIMENTO.ENTITIES
+{
+    public class Paciente : EntityBase
+    {
+        public bool IsValid { get; private set; }
+        public List<Tuple<string, string>> Erros { get; private set; }
+        public Paciente(Guid _id, String _nome, String _sobrenome, String _cpf)
+        {
+            _id = ID;
+            _nome = Nome;
+            _sobrenome = Sobrenome;
+            _cpf = CPF;
+            IsValid = true;
+            if (Nome.Length < 2 || Nome.Length > 15)
+            {
+                Erros.Add(new Tuple<string, string>("Nome", "O campo Nome não pode ser menor que 2 ou maior que 15!"));
+                IsValid = false;
+            }
+            if (Sobrenome.Length < 5 || Sobrenome.Length > 40)
+            {
+                Erros.Add(new Tuple<string, string>("Sobrenome", "O campo Sobrenome não pode ser menor que 5 ou maior que 40!"));
+                IsValid = false;
+            }
+            if (CPF.Length == 0 || CPF.Length > 11)
+            {
+                Erros.Add(new Tuple<string, string>("CPF", "Insira um CPF válido!"));
+                IsValid = false;
+            }
+        }
+        public string CPF { get; private set; }
+        [DataType(DataType.Date)]
+        public DateTime DataNasc { get; private set; }
+        public string Endereco { get; private set; }
+    }
+}
---
+++ ATENDIMENTO/SHAREDCONTEXT/ENTITYBASE.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SHAREDCONTEXT
+{
+    public class EntityBase
+    {
+        public EntityBase()
+        {
+            ID = Guid.NewGuid();
+        }
+
+        public EntityBase(Guid id, string nome, string sobrenome)
+        {
+            ID = id;
+            Nome = nome;
+            Sobrenome = sobrenome;
+        }
+        public Guid ID { get; private set; }
+        public string Nome { get; private set; }
+        public string Sobrenome { get; private set; }
+    }
+}
